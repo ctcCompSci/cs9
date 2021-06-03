5b605bf7cfb42251d6db9620135597a947783639
Jeff <jtindell2@swordofthespirit.org>
2021-06-03 10:56:36 -0400

    updated

---
+++ games/submersed/main.lua
@@ -0,0 +1,17 @@
+debug = true
+
+playerImg = nil -- this is just for storage
+
+function love.load(arg)
+    -- playerImg = love.graphics.newImage('assets/plane.png')
+    --we now have an asset ready to be used inside Love
+end
+
+function love.update(dt)
+
+end
+
+
+function love.draw(dt)
+    -- love.graphics.draw(playerImg, 100, 100)
+end
